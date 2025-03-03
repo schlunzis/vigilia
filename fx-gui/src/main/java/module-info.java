8ba126f0eb623759fcd67170d361152eb1e777a2
Jonas Pohl <jonas.jp4c@gmail.com>
2025-03-03 17:03:25 +0100

    First steps in fx gui

---
+++ fx-gui/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module vigilia.gui.fx {
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires java.net.http;
+    requires org.openapitools.jackson.nullable;
+    requires jakarta.annotation;
+    requires com.fasterxml.jackson.databind;
+
+    opens org.schlunzis.vigilia.gui.fx.model to com.fasterxml.jackson.databind;
+
+    exports org.schlunzis.vigilia.gui.fx;
+}
--- fx-gui/src/main/java/org/schlunzis/vigilia/gui/fx/Main.java
+++ fx-gui/src/main/java/org/schlunzis/vigilia/gui/fx/Main.java
@@ -1,9 +1,12 @@
 package org.schlunzis.vigilia.gui.fx;
 
+import org.schlunzis.vigilia.gui.fx.api.DefaultApi;
+
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws ApiException {
+        DefaultApi api = new DefaultApi();
+        api.searchFiles("Hello World").forEach(System.out::println);
     }
 
 }
