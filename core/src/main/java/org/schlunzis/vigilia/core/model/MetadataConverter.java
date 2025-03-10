9f6818fab40b8587e167883e866d5f8b782b342c
Jonas Pohl <jonas.jp4c@gmail.com>
2025-03-10 16:33:34 +0100

    Persisting embeddings in derby db

---
+++ core/src/main/java/org/schlunzis/vigilia/core/model/MetadataConverter.java
@@ -0,0 +1,34 @@
+package org.schlunzis.vigilia.core.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Converter
+public class MetadataConverter implements AttributeConverter<Map<String, Object>, String> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(Map<String, Object> attribute) {
+        try {
+            return objectMapper.writeValueAsString(attribute);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error converting map to JSON string.", e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> convertToEntityAttribute(String s) {
+        try {
+            return objectMapper.readValue(s, HashMap.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Error converting JSON string to map.", e);
+        }
+    }
+}
---
+++ example.md
@@ -0,0 +1,15 @@
+# Cheese
+
+there are many different types of cheese:
+
+- cheddar
+- brie
+- gouda
+- feta
+- blue
+- and many more.
+
+# Dolphins
+
+Dolphins are hunting fish for a healthy diet. They are very intelligent and social animals. They are also known for
+their playful behavior.
