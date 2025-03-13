f0cf9ccdea53fc61929090b3752178be96883bc5
Jonas Pohl <jonas.jp4c@gmail.com>
2025-03-13 09:55:33 +0100

    Persisting embeddings in derby db (#18)
    * Persisting embeddings in derby db
    
    * Adding missing dependency
    
    * Applying review comments

--- core/src/main/java/org/schlunzis/vigilia/core/embedding/EmbeddingWrapper.java
+++ core/src/main/java/org/schlunzis/vigilia/core/embedding/EmbeddingWrapper.java
@@ -4,6 +4,4 @@ import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.data.segment.TextSegment;
 
 public record EmbeddingWrapper(Embedding embedding, TextSegment textSegment) {
-
-
 }
