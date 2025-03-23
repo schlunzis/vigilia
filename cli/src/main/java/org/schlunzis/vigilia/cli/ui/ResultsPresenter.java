e29f8c614e428cde581bd31a0109945685367330
Tilman Holube <tilman@holube.de>
2025-03-23 16:26:56 +0100

    colored score

--- cli/src/main/java/org/schlunzis/vigilia/cli/ui/ResultsPresenter.java
+++ cli/src/main/java/org/schlunzis/vigilia/cli/ui/ResultsPresenter.java
@@ -2,6 +2,7 @@ package org.schlunzis.vigilia.cli.ui;
 
 import lombok.CustomLog;
 import org.schlunzis.vigilia.cli.model.SearchResultDTO;
+import org.schlunzis.vigilia.cli.util.ColorSelector;
 import picocli.CommandLine;
 
 import java.util.Arrays;
@@ -11,9 +12,17 @@ import java.util.stream.Collectors;
 @CustomLog
 public class ResultsPresenter {
 
-    private CommandLine.Help.Ansi ansi = CommandLine.Help.Ansi.AUTO;
+    private static final CommandLine.Help.Ansi ansi = CommandLine.Help.Ansi.AUTO;
+
+    private double minScore = 0.0;
+    private double maxScore = 0.0;
 
     public void presentResults(List<SearchResultDTO> results) {
+        //noinspection DataFlowIssue
+        minScore = results.stream().mapToDouble(SearchResultDTO::getScore).min().orElse(0.0);
+        //noinspection DataFlowIssue
+        maxScore = results.stream().mapToDouble(SearchResultDTO::getScore).max().orElse(0.0);
+
         for (int i = 0; i < results.size() - 1; i++) {
             presentResult(results.get(i));
             printSeparator();
@@ -25,7 +34,10 @@ public class ResultsPresenter {
     }
 
     private void presentResult(SearchResultDTO result) {
-        String resultDisplay = ansi.string("@|bold,underline " + result.getScore() + ": " + result.getPath() + "|@ ");
+        @SuppressWarnings("DataFlowIssue") double score = result.getScore();
+        String scoreColor = ColorSelector.getColor(minScore, maxScore, score);
+        String formattedScore = String.format("%.3f", score);
+        String resultDisplay = ansi.string("@|" + scoreColor + " " + formattedScore + "|@: @|bold,underline " + result.getPath() + "|@ ");
         log.log(resultDisplay);
         if (result.getText() == null || result.getText().isBlank())
             return;
