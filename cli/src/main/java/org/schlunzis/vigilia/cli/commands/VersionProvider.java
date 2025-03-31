1c139385f86d7375b76b672bd3c25b243b8b425d
Tilman Holube <tilman@holube.de>
2025-03-31 13:54:58 +0200

    provide version via system property (#57)

--- cli/src/main/java/org/schlunzis/vigilia/cli/commands/VersionProvider.java
+++ cli/src/main/java/org/schlunzis/vigilia/cli/commands/VersionProvider.java
@@ -1,13 +1,12 @@
 package org.schlunzis.vigilia.cli.commands;
 
-import org.schlunzis.vigilia.cli.CLIApplication;
 import picocli.CommandLine;
 
 public class VersionProvider implements CommandLine.IVersionProvider {
 
     @Override
     public String[] getVersion() {
-        return new String[]{CLIApplication.class.getPackage().getImplementationVersion()};
+        return new String[]{System.getProperty("vigilia.version")};
     }
 
 }
